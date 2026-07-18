b822183d6fdb687a69d6c385354dfceda2807de7
850525344@qq.com <850525344@qq.com>
2026-07-18 10:55:38 +0800

    解决course8打包报错

--- app/[local]/(course)/course8/page.tsx
+++ app/[local]/(course)/course8/page.tsx
@@ -1,4 +1,4 @@
-'use cache'
+import { Suspense } from 'react'
 import SearchBar from '@/components/SearchBar'
 import ChangeTheme from './components/changeTheme'
 import { FormDemo } from './components/FormDemo'
@@ -17,20 +17,22 @@ type Props = {
 	searchParams: Promise<{ query?: string; page?: string }>
 }
 
-export async function generateStaticParams() {
-	return [{ local: 'zh' }]
-}
+// export async function generateStaticParams() {
+// 	return [{ local: 'zh' }]
+// }
 
-export default async function Course8Page({ searchParams }: Props) {
-	const { query = '', page = '1' } = await searchParams
-	console.log('Course 8 searchParams:', { query, page })
+export default function Course8Page() {
+	// const { query = '', page = '1' } = await searchParams
+	// console.log('Course 8 searchParams:', { query, page })
 	// 直接在服务端请求数据，SEO 友好，无 Loading 闪烁
 	//   const products = await fetchProducts(query, Number(page))
 	return (
 		<div className="p-4 rounded-xl bg-white shadow-lg transition-colors duration-200 dark:bg-slate-800 dark:border dark:border-slate-700 border-red-100 border">
 			<h1>Course 8</h1>
 			<div className="w-[500px]">
-				<SearchBar />
+				<Suspense>
+					<SearchBar />
+				</Suspense>
 			</div>
 			<div className="mt-4">
 				<ChangeTheme />

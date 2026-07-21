781a9231a3b7e43bf6a8d0a538e9d2c5f9dc5155
850525344@qq.com <850525344@qq.com>
2026-07-21 09:33:28 +0800

    发送交易转账

--- ethers/helloVitalik/signer-total-code.mjs
+++ ethers/helloVitalik/signer-total-code.mjs
@@ -1,28 +1,41 @@
 import 'dotenv/config'
 import { ethers } from 'ethers'
 
-// Signer 签名者类和它派生的 Wallet 钱包类，并利用它来发送 ETH
+// 利用Alchemy的rpc节点连接以太坊测试网络
+const ALCHEMY_SEPOLIA_URL = process.env.ALCHEMY_NETWORK_ADDERSS
+const provider = new ethers.JsonRpcProvider(ALCHEMY_SEPOLIA_URL)
 
-const SEPOLIA_RPC = process.env.ALCHEMY_NETWORK_ADDERSS
-
-const provider = new ethers.JsonRpcProvider(SEPOLIA_RPC)
-
-// 方法 1：创建随机的 wallet 对象
 // 创建随机的wallet对象
 const wallet1 = ethers.Wallet.createRandom()
 const wallet1WithProvider = wallet1.connect(provider)
+const mnemonic = wallet1.mnemonic // 获取助记词
 
-// 方法 2：用私钥创建 wallet 对象
 // 利用私钥和provider创建wallet对象
 const privateKey = process.env.PRIVATE_KEY
 const wallet2 = new ethers.Wallet(privateKey, provider)
 
-//发送交易，获得收据
-const send = async () => {
-	// 5. 发送ETH
-	// 如果这个钱包没sepolia测试网ETH了，去水龙头领一些
-	// 1. https://faucets.chain.link/
+// 从助记词创建wallet对象
+const wallet3 = ethers.Wallet.fromPhrase(mnemonic.phrase)
 
+async function getWalletAddresses() {
+	const address1 = await wallet1WithProvider.getAddress()
+	const address2 = await wallet2.getAddress()
+	const address3 = await wallet3.getAddress()
+
+	console.log(`1. 获取钱包地址`)
+	console.log(`钱包1地址: ${address1}`)
+	console.log(`钱包2地址: ${address2}`)
+	console.log(`钱包3地址: ${address3}`)
+	console.log(`钱包1和钱包3的地址是否相同: ${address1 === address3}`)
+	console.log(`钱包1助记词: ${wallet1.mnemonic.phrase}`)
+	console.log(`钱包2私钥: ${wallet2.privateKey}`)
+	const txCount1 = await provider.getTransactionCount(wallet1WithProvider)
+	const txCount2 = await provider.getTransactionCount(wallet2)
+	console.log(`钱包1发送交易次数: ${txCount1}`)
+	console.log(`钱包2发送交易次数: ${txCount2}`)
+	// 5. 发送ETH
+	// 如果这个钱包没sepolia测试网ETH了，去水龙头领一些，钱包地址: 0xe16C1623c1AA7D919cd2241d8b36d9E79C1Be2A2
+	// 1. https://faucets.chain.link/
 	console.log(`\n5. 发送ETH（测试网）`)
 	// i. 打印交易前余额
 	console.log(`i. 发送前余额`)
@@ -32,7 +45,6 @@ const send = async () => {
 	console.log(
 		`钱包2: ${ethers.formatEther(await provider.getBalance(wallet2))} ETH`
 	)
-	const address1 = await wallet1.getAddress()
 	// ii. 构造交易请求，参数：to为接收地址，value为ETH数额
 	const tx = {
 		to: address1,
@@ -51,23 +63,6 @@ const send = async () => {
 	console.log(
 		`钱包2: ${ethers.formatEther(await provider.getBalance(wallet2))} ETH`
 	)
-	// 创建交易请求，参数：to为接收地址，value为ETH数额
 }
 
-// 获取钱包助记词
-const getPhrase = async () => {
-	const wallet1WithProvider = wallet1.connect(provider)
-	// const mnemonic = wallet1.mnemonic // 获取助记词
-
-	const address1 = await wallet1.getAddress()
-	console.log('wallet address', address1)
-
-	console.log(`钱包2私钥: ${wallet2.privateKey}`)
-
-	const txCount1 = await provider.getTransactionCount(wallet1WithProvider)
-	const txCount2 = await provider.getTransactionCount(wallet2)
-	console.log(`钱包1发送交易次数: ${txCount1}`)
-	console.log(`钱包2发送交易次数: ${txCount2}`)
-}
-
-send()
+getWalletAddresses()

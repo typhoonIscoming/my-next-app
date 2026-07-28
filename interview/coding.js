a450fbfc87de6d0aa2823c8e0ab82c8d40a16901
850525344@qq.com <850525344@qq.com>
2026-07-28 16:36:05 +0800

    coding

--- interview/coding.js
+++ interview/coding.js
@@ -66,7 +66,7 @@ const curry = (...args) => {
 	return addFn;
 };
 
-console.log(curry(1, 2, 3)(4)(5, 6)(7, 8, 9).valueOf());
+// console.log(curry(1, 2, 3)(4)(5, 6)(7, 8, 9).valueOf());
 
 const list = [
 	{ id: 'a2', label: '1', pid: 'a1' },
@@ -98,4 +98,78 @@ const buildTree = (node, tree) => {
 };
 
 const tree = buildTree({ id: 'root', name: 'root', pid: null }, list);
-console.log(tree);
+// console.log(tree);
+
+// 思路2：斐波那契数列
+var numWays2 = function (n) {
+	let a = 0;
+	let b = 0;
+	let result = 1;
+	for (let i = 1; i <= n; ++i) {
+		a = b;
+		b = result;
+		result = a + b;
+	}
+	return result;
+};
+
+// console.log(numWays2(4));
+
+var lengthOfLongestSubstring = function (s) {
+	let arr = [];
+	let max = 0;
+	for (let i = 0, len = s.length; i < len; ++i) {
+		const sameIndex = arr.findIndex((item) => item === s[i]);
+		arr.push(s[i]);
+		if (sameIndex > -1) {
+			arr = arr.splice(sameIndex + 1);
+		}
+		max = Math.max(arr.length, max);
+	}
+	return max;
+};
+
+var canPermutePalindrome = function (s) {
+	const set = new Set();
+	s.split('').forEach((key) => {
+		console.log('===', key, set);
+		if (set.has(key)) {
+			set.delete(key);
+		} else {
+			set.add(key);
+		}
+		console.log('操作完后==', key, set);
+	});
+	return set.size <= 1;
+};
+
+console.log('回文', lengthOfLongestSubstring('ab111cdefedcba'));
+
+function permute(arr) {
+	const result = []; // ⽤于存储⽣成的全排列
+	function backtrack(subarr, remaining) {
+		// 如果没有剩余元素，当期排列就是⼀个全排列
+		if (remaining.length === 0) {
+			console.log('====', subarr);
+			result.push(subarr.slice()); // 将当前排列添加到结果数组
+		} else {
+			for (let i = 0; i < remaining.length; i++) {
+				subarr.push(remaining[i]); // 将当前元素添加到排列
+				const newRemaining = [
+					...remaining.slice(0, i),
+					...remaining.slice(i + 1),
+				]; // ⽣成新的剩余元素数组
+				console.log('subarr, newRemaining', subarr, newRemaining);
+				backtrack(subarr, newRemaining); // 递归⽣成剩余元素的排列
+				subarr.pop(); // 回溯，移除刚添加的元素，以尝试其他排列⽅式
+				console.log('回溯', subarr);
+			}
+		}
+	}
+	// 调⽤回溯函数开始⽣成全排列
+	backtrack([], arr);
+	return result; // 返回所有⽣成的全排列
+}
+const inputArray = [1, 2, 3];
+const permutations = permute(inputArray);
+console.log(permutations);
